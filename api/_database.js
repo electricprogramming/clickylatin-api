1fb582e4c1588c178a5853fa716fe1b1b9a1eb51
electricprogramming <yeungj316@gmail.com>
2025-04-17 11:09:46 -0400

    alr

--- api/_database.js
+++ api/_database.js
@@ -2,7 +2,7 @@ export async function getData() {
   return new Promise((resolve, reject) => {
     fetch('https://api.github.com/repos/electricprogramming/clickylatin-database/contents/data.json', {
       headers: {
-        'Authorization': `token ${process.env.GITHUB_TOKEN}`
+        'Authorization': `token ${process.env.GITHUB_API_KEY}`
       }
     })
       .then(res => res.json())
@@ -13,24 +13,24 @@ export async function getData() {
   });
 }
 
-export async function updateFileContent(newContent) {
+export async function setData(newData) {
   return new Promise((resolve, reject) => {
     fetch('https://api.github.com/repos/electricprogramming/clickylatin-database/contents/data.json', {
       headers: {
-        'Authorization': `token ${process.env.GITHUB_TOKEN}`
+        'Authorization': `token ${process.env.GITHUB_API_KEY}`
       }
     })
       .then(res => res.json())
       .then(data => fetch('https://api.github.com/repos/electricprogramming/clickylatin-database/contents/data.json', {
         method: 'PUT',
         headers: {
-          'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+          'Authorization': `token ${process.env.GITHUB_API_KEY}`,
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           message: 'remotely update data.json',
           sha: data.sha,
-          content: btoa(JSON.stringify(newContent))
+          content: btoa(JSON.stringify(newData))
         })
       }))
       .then(res => res.json())
---
+++ api/get-game.js
@@ -0,0 +1,14 @@
+import { getData } from './_database.js';
+export default async function get_game(req, res) {
+  const id = req.query.id;
+  if (typeof id !== 'string') {
+    return res.status(400).json({ error: 'ID must be a string' });
+  }
+  getData().then(data => {
+    if (id in data) {
+      res.status(200).json(data[id]); // Send JSON data
+    } else {
+      res.status(404).json({ error: 'not found' });
+    }
+  });
+}
