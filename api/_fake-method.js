9684b6147c917df69f525dc94ed72140bc6bade2
electricprogramming <yeungj316@gmail.com>
2025-04-17 15:52:07 -0400

    alr

--- api/_fake-method.js
+++ api/_fake-method.js
@@ -1,8 +1,8 @@
 import cors from './_cors.js';
 export default function fake_method(req, res) {
+  cors(res);
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  cors(res);
   res.status(404).send(`Cannot ${req.method} ${req.url}`);
 }
