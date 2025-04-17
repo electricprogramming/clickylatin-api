30f529179505bc64dc9618611eb5c98c94140223
electricprogramming <yeungj316@gmail.com>
2025-04-17 16:05:42 -0400

    !!!

--- api/create-game.js
+++ api/create-game.js
@@ -1,6 +1,6 @@
 import cors from './_cors.js';
 import { getData, setData } from './_database.js';
-export default async function delete_game(req, res) {
+export default async function create_game(req, res) {
   cors(res);
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
