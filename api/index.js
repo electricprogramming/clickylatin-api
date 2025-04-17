bf185f1335b0c19152b76457ba10e7612941025b
electricprogramming <yeungj316@gmail.com>
2025-04-17 15:40:36 -0400

    status 200

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 import cors from './_cors.js';
 export default function index(req, res) {
   cors(res);
-  res.send(`Welcome to the new Clicky Latin API!
+  res.status(200).send(`Welcome to the new Clicky Latin API!
 To get the data of a game, GET /get-game. To get all games, GET /all.
 To create a game, POST /create-game. To delete a game, GET /delete with authentication.
 To get the current deployment, GET /current-deployment. To get the message of the current deployment, GET /current-deployment-msg.
 To suggest a new feature, POST /feature-suggestion.`);
-res.send('abcdef');
 }
