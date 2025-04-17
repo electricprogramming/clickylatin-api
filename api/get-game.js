56b73dc7a8cc3f904f803de6b7960762a403f12d
electricprogramming <yeungj316@gmail.com>
2025-04-17 11:11:17 -0400

    boom

--- api/get-game.js
+++ api/get-game.js
@@ -1,12 +1,12 @@
 import { getData } from './_database.js';
 export default async function get_game(req, res) {
-  const id = req.query.gamecode;
-  if (typeof id !== 'string') {
-    return res.status(400).json({ error: 'ID must be a string' });
+  const gameCode = req.query.gamecode;
+  if (!gameCode) {
+    return res.status(400).json({ error: 'Game code does not exist.' });
   }
   getData().then(data => {
-    if (id in data) {
-      res.status(200).json(data[id]); // Send JSON data
+    if (gameCode in data) {
+      res.status(200).json(data[gameCode]); // Send JSON data
     } else {
       res.status(404).json({ error: 'not found' });
     }
