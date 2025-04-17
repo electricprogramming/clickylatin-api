8ccafff78237c231a8eb76c8fead205a7f4ad82a
electricprogramming <yeungj316@gmail.com>
2025-04-17 16:12:08 -0400

    date

--- api/create-game.js
+++ api/create-game.js
@@ -18,7 +18,17 @@ export default async function create_game(req, res) {
       newId = Math.round(Math.random() * 90000000) + 10000000;
     }
     data[newId] = newData;
-    console.log(`New game created with id ${newId} and data\n${JSON.stringify(newData, null, 2)}\nat ${getFormattedDate()}`);
+    const formattedDate = new Date().toLocaleString('en-US', {
+      timeZone: 'America/New_York',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    });
+    console.log(`New game created with id ${newId} and data\n${JSON.stringify(newData, null, 2)}\nat ${formattedDate}`);
     setData(data)
       .then(() => {
         res.status(200).json({ message: 'Success!', gameCode: newId })
