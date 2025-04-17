5e073ee2a35af8121287d6de950466a2eb86f59e
electricprogramming <yeungj316@gmail.com>
2025-04-17 12:35:01 -0400

    please!!!

--- api/delete-game.js
+++ api/delete-game.js
@@ -14,11 +14,11 @@ export default async function delete_game(req, res) {
     return res.status(400).json({ error: 'Game code does not exist.' });
   }
   getData().then(data => {
-    if (data.hasOwnProperty(id)) {
-      delete data[id];
+    if (data.hasOwnProperty(gameCode)) {
+      delete data[gameCode];
       setData(data)
         .then(thing => {
-          res.status(200).json({ message: `Game with id ${id} deleted successfully`, thing });
+          res.status(200).json({ message: `Game with id ${gameCode} deleted successfully`, thing });
         })
         .catch(e => res.status(500).json({ error: e.message }));
     } else {
