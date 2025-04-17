79a713e7aa6245a5dda428352fb32f260a00b4bd
electricprogramming <yeungj316@gmail.com>
2025-04-17 15:38:01 -0400

    methods

---
+++ api/index.js
@@ -0,0 +1,3 @@
+export default function index(req, res) {
+  res.send(`Welcome to the new Clicky Latin API! To get the data of a game, GET /get-game. To create a game, POST /create-game. To get all games, GET /all. To delete a game, GET or POST /delete with authentication. To get the current deployment, GET /current-deployment. To get the message of the current deployment, GET /current-deployment-msg. To suggest a feature, POST /feature-suggestion`)
+}
