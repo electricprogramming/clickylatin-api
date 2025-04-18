debb8463a814a4056de13f47052064e1933ef670
electricprogramming <yeungj316@gmail.com>
2025-04-18 10:49:59 -0400

    credentials

--- api/_cors.js
+++ api/_cors.js
@@ -2,4 +2,5 @@ export default function cors(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
